edfebe8bb54072c89214f1d5ecbd673da281c86f
sciangulahugo <sciangulahugo@gmail.com>
2023-08-09 10:39:42 -0300

    v1;

--- controllers/client.php
+++ controllers/client.php
@@ -1,5 +1,55 @@
 <?php
+
 require "../models/Client.php";
+
+// // var_dump($_SERVER['REQUEST_METHOD']);
+
+// switch ($_SERVER['REQUEST_METHOD']) {
+//     case 'GET':
+//         return listarCliente();
+//     case 'POST':
+//         return crearCliente();
+//     default:
+//         http_response_code(400);
+//         header('Content-Type: application/json');
+//         echo (json_encode(["message" => "Bad Request"]));
+//         break;
+// }
+
+// function listarCliente()
+// {
+//     $client = new Client();
+//     $response = $client->list();
+//     header('Content-Type: application/json');
+//     http_response_code(200);
+//     echo json_encode($response);
+// }
+
+// function crearCliente()
+// {
+//     try {
+//         // Tomar del body, los datos
+//         $firstName = $_POST["firstName"] ?? "";
+//         $lastName = $_POST["lastName"] ?? "";
+//         var_dump($_REQUEST);
+//         return;
+//         if (empty($firstName) || empty($lastName)) {
+//             throw new Exception("lastName and firstName required");
+//         }
+
+
+//         $client = new Client();
+//         // $client->create();
+//     } catch (Exception $e) {
+//         header('Content-Type: application/json');
+//         http_response_code(400);
+//         echo json_encode(["message" => $e->getMessage()]);
+//     }
+// }
+
+
+
+// die();
 
 $firstName = isset($_POST["firstName"]) ? $_POST["firstName"] : "";
 $lastName = isset($_POST["lastName"]) ? $_POST["lastName"] : "";
--- views/scripts/clients.js
+++ views/scripts/clients.js
@@ -26,48 +26,48 @@ function createClient(e) {
     // console.log(lastName);
 
     // Guardamos con ajax
-    setTimeout(() => {
-        $.ajax({
-            url: "../controllers/client.php?op=create",
-            type: "POST",
-            data: formData,
-            contentType: false,
-            processData: false,
+    // setTimeout(() => {
+    $.ajax({
+        url: "../controllers/client.php?op=create",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
 
-            success: function (data) {
-                // console.log(data);
-                // Cerrar la ventana modal
-                $("#modal").modal('hide');
-                // Limpiar los campos del formulario
-                $("#form")[0].reset();
-                // document.getElementById("form").reset();
+        success: function (data) {
+            // console.log(data);
+            // Cerrar la ventana modal
+            $("#modal").modal('hide');
+            // Limpiar los campos del formulario
+            $("#form")[0].reset();
+            // document.getElementById("form").reset();
 
-                // Actualizar el contenido del toast con el mensaje de error
-                $(".toast").removeClass("text-bg-danger");
-                $(".toast").addClass("text-bg-success");
-                $(".toast .toast-body").text(data);
-                $(".toast").toast("show");
+            // Actualizar el contenido del toast con el mensaje de error
+            $(".toast").removeClass("text-bg-danger");
+            $(".toast").addClass("text-bg-success");
+            $(".toast .toast-body").text(data);
+            $(".toast").toast("show");
 
-                loader.addClass("visually-hidden");
+            loader.addClass("visually-hidden");
 
-                // Mostramos todos los clientes
-                list();
-            },
-            error: function (xhr, status, error) {
-                const errorMessage = xhr.responseText; // Obtener el mensaje de error
-                loader.addClass("visually-hidden");
+            // Mostramos todos los clientes
+            list();
+        },
+        error: function (xhr, status, error) {
+            const errorMessage = xhr.responseText; // Obtener el mensaje de error
+            loader.addClass("visually-hidden");
 
-                // Actualizar el contenido del toast con el mensaje de error
-                $(".toast").removeClass("text-bg-success");
-                $(".toast").addClass("text-bg-danger");
-                $(".toast .toast-body").text(errorMessage);
-                $(".toast").toast("show");
+            // Actualizar el contenido del toast con el mensaje de error
+            $(".toast").removeClass("text-bg-success");
+            $(".toast").addClass("text-bg-danger");
+            $(".toast .toast-body").text(errorMessage);
+            $(".toast").toast("show");
 
-                // Cerramos el modal
-                $("#modal").modal("hide");
-            }
-        });
-    }, [2000]);
+            // Cerramos el modal
+            $("#modal").modal("hide");
+        }
+    });
+    // }, [2000]);
 
 }
 
@@ -80,39 +80,39 @@ function list() {
     loader.removeClass("visually-hidden");
 
     // Hacemos una peticion de ajax
-    setTimeout(() => {
-        $.ajax({
-            url: "../controllers/client.php?op=list",
-            type: "GET",
-            success: function (data) {
-                table.removeClass("opacity-25");
-                table.removeClass("visually-hidden");
-                loader.addClass("visually-hidden");
-                // Parseamos los datos
-                const clients = JSON.parse(data);
-                // const clients = [{
-                //     id: 1,
-                //     firsName: "Hugo",
-                //     lastName: "Sciangula"
-                // }]
-                // Seleccionamos el cuerpo de la tabla
-                let tbody = $("#table tbody");
+    // setTimeout(() => {
+    $.ajax({
+        url: "../controllers/client.php?op=list",
+        type: "GET",
+        success: function (data) {
+            table.removeClass("opacity-25");
+            table.removeClass("visually-hidden");
+            loader.addClass("visually-hidden");
+            // Parseamos los datos
+            const clients = JSON.parse(data);
+            // const clients = [{
+            //     id: 1,
+            //     firsName: "Hugo",
+            //     lastName: "Sciangula"
+            // }]
+            // Seleccionamos el cuerpo de la tabla
+            let tbody = $("#table tbody");
 
-                // Limpiamos antes de mostrar
-                tbody.empty();
+            // Limpiamos antes de mostrar
+            tbody.empty();
 
-                // Insertamos los datos
-                for (let i = 0; i < clients.length; i++) {
-                    let client = clients[i];
-                    let row = $("<tr></tr>").appendTo(tbody);
-                    $("<th scope=\"row\">" + client.id + " </th>").appendTo(row);
-                    $("<td>" + client.firstName + " </td>").appendTo(row);
-                    $("<td>" + client.lastName + " </td>").appendTo(row);
-                    $('<td><a href="#"><i class="bi bi-eye" onclick={activitiesByClient(' + client.id + ')}></i></a></td>').appendTo(row);
-                }
+            // Insertamos los datos
+            for (let i = 0; i < clients.length; i++) {
+                let client = clients[i];
+                let row = $("<tr></tr>").appendTo(tbody);
+                $("<th scope=\"row\">" + client.id + " </th>").appendTo(row);
+                $("<td>" + client.firstName + " </td>").appendTo(row);
+                $("<td>" + client.lastName + " </td>").appendTo(row);
+                $('<td><a href="#"><i class="bi bi-eye" onclick={activitiesByClient(' + client.id + ')}></i></a></td>').appendTo(row);
             }
-        });
-    }, [2000]);
+        }
+    });
+    // }, [2000]);
 }
 
 function activitiesByClient(id) {
@@ -125,33 +125,33 @@ function activitiesByClient(id) {
     tableSkeleton.removeClass("visually-hidden");
     tableDetail.addClass("visually-hidden");
 
-    setTimeout(() => {
-        $.ajax({
-            url: "../controllers/activity.php?op=findbyclient",
-            type: "POST",
-            data: { clientId: id },
-            success: function (data) {
-                const activities = JSON.parse(data);
-                tableSkeleton.addClass("visually-hidden");
-                tableDetail.removeClass("visually-hidden");
-                const tbody = $("#tableDetail tbody");
-                tbody.empty();
-                let total = 0;
-                for (let i = 0; i < activities.length; i++) {
-                    let activity = activities[i];
-                    total += Number(activity.amount);
-                    let row = $("<tr></tr>").appendTo(tbody);
-                    $("<td>" + activity.date + " </td>").appendTo(row);
-                    $("<td>" + activity.invoiceId + " </td>").appendTo(row);
-                    $("<td>$" + activity.amount + " </td>").appendTo(row);
-                }
+    // setTimeout(() => {
+    $.ajax({
+        url: "../controllers/activity.php?op=findbyclient",
+        type: "POST",
+        data: { clientId: id },
+        success: function (data) {
+            const activities = JSON.parse(data);
+            tableSkeleton.addClass("visually-hidden");
+            tableDetail.removeClass("visually-hidden");
+            const tbody = $("#tableDetail tbody");
+            tbody.empty();
+            let total = 0;
+            for (let i = 0; i < activities.length; i++) {
+                let activity = activities[i];
+                total += Number(activity.amount);
                 let row = $("<tr></tr>").appendTo(tbody);
-                $("<td colspan=\"2\" class=\"text-end\">Total</td>").appendTo(row);
-                $("<td>$" + total + " </td>").appendTo(row);
-
+                $("<td>" + activity.date + " </td>").appendTo(row);
+                $("<td>" + activity.invoiceId + " </td>").appendTo(row);
+                $("<td>$" + activity.amount + " </td>").appendTo(row);
             }
-        });
-    }, [2000]);
+            let row = $("<tr></tr>").appendTo(tbody);
+            $("<td colspan=\"2\" class=\"text-end\">Total</td>").appendTo(row);
+            $("<td>$" + total + " </td>").appendTo(row);
+
+        }
+    });
+    // }, [2000]);
 }
 
 init();
